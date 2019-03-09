c09328982647e867530bd8c48910dab2cb4cbf8d
yutakasaito <fvkibdcz7@yahoo.co.jp>
2019-03-09 19:35:48 +0900

    モンスターの階段との衝突・接触が認識されない。
    groundのPrefabには、colliderｺﾝﾎﾟｰﾈﾝﾄをｱﾀｯﾁできた。
    Ladderのprefabにも、colliderをｱﾀｯﾁしたが、DemoSceneのLadder(s)に反映されない。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -17,49 +17,40 @@ public class PlayerController : MonoBehaviour
     {       //FixedUpdateにしても変わらずに回ってしまうのだが・・・
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.Rotate(0, -1, 0);
+            this.transform.Rotate(0, -2, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.Rotate(0, 1, 0);
+            this.transform.Rotate(0, 2, 0);
         }
-
-
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             // ローカルX軸正方向に移動
-            //ローカルY軸の回転を制限
-            this.myRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
-            //次のは壁にぶつかった後、ずっと回転してしまう。
-            if (this.myRigidbody.velocity.magnitude < 2.0f)
+           // this.myRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+            if (this.myRigidbody.velocity.magnitude < 2.0f)     //速度制限
             {
                 this.myRigidbody.AddForce(transform.TransformDirection(Vector3.forward) * 0.25f, ForceMode.Impulse);
-                //this.myRigidbody.AddForce(transform.TransformDirection(Vector3.forward)*0.25f, ForceMode.Acceleration); Accelerationでは動かず
-                //this.myRigidbody.AddForce(transform.forward*Input.GetAxisRaw("Horizontal")*0.5f,ForceMode.Impulse);動かず
-                //transform.right ローカル座標のX軸   transform.up ローカル座標のY軸  transform.forward ローカル座標のZ軸
-                //Input.GetAxisRaw(string axisName)      特定方向の入力を - 1 ～ 1 の正規化された値で返す
-                //  Input.GetAxis("Vertical")           垂直方向キー
-                // Input.GetAxis("Horizontal")         水平方向キー
-                //動いている間、FreezeRotation.yをtrueににすればいいのではないか？ }
+            }
+        }       
+    }
+    //衝突処理
+        void OntriggerStay(Collider other)     //OnTriggerEnterでは衝突した瞬間だけしか働かないだろう、接触中みたいな関数がないとうまくいかないだろう　→　OnTriggerStayというのがあった！
+        {
+            //障害物に衝突
+            //まず、はしごに衝突したら上か下にいく。前に少し進める　　→　難しそう！はしごの上下判定も必要。さらに組み合わせのあるはしごもある
+            //自動的に上or下にワープするのはかえって難しい感じ。単純にUpForce、DownForceを使ってみるか
+            if (other.gameObject.tag == "Ladder")  
+            {
+                Debug.Log("LADDER!");
+                //はしごの大きさを取得したい！
+                //positionを↑or↓、且つ、前に
+                //this.myRigidbody.transform.position=new Vector3(     )
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    this.myRigidbody.AddForce(transform.TransformDirection(Vector3.up) * 0.1f, ForceMode.Impulse);
+                }
             }
         }
-        else
-        {
-            this.myRigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-            //まだ回転してしまうことはあるが、上矢印ｷｰでとまるようにはなった。
-        }
-    }
 }
 
-
-//ｱﾄﾞﾌｫ-ｽのﾍﾞｸﾀ-3の力にtransform.forwardなどを掛けると、その方向にﾌｫ-ｽがかかり加速して行きます。
-
-//ﾌｫ-ｽﾓ-ﾄﾞはアクセラレーションの方が楽です。
-
-//clamp(与えたい加速度 , 希望最高速度-現在速度[+-両方])
-//しておくことを忘れないようにしましょう。
-//速度コントロールが楽になりますね。
-//[既実行ならすみません]
-
-//あとこれらは全て、fixupdateの中で行うと正しい動作をします。
